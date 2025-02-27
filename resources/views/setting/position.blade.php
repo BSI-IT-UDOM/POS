5910d29cc1afb9488a1b8e80fde70fd06a2f2a30
BSI-IT-UDOM <it@bsi.com.kh>
2025-02-27 11:46:06 +0700

    first submit

---
+++ resources/views/setting/position.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.setting')
+
+@section('content')
+
+<div class="flex flex-col">
+  <div class="bg-background flex flex-col items-center flex-grow px-4 md:px-0 mt-2">
+    <div class="flex flex-col md:flex-row justify-between items-center w-full md:w-4/5">
+      <a href="#" id="createPosition" class="bg-green-500 text-white font-bold py-1 px-4 md:py-2 md:px-6 rounded flex items-center text-sm md:text-base">CREATE</a>
+      <div class="relative flex w-full md:w-auto">
+        <form id="searchForm" method="GET" class="w-full md:w-auto flex items-center">
+          <input id="searchInput" type="text" placeholder="Search..." class="border border-input rounded-full py-1 px-4 pl-10 w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-primary"  />
+          <button type="submit" class="bg-gray-200 rounded-full py-1 px-4 absolute right-0 top-0 mt-1 mr-2 flex items-center justify-center">
+            <i class="fas fa-search text-gray-500"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+    <div class="w-full md:w-4/5 border-2 border-bsicolor p-2 font-times">
+      <div class="overflow-x-auto w-full">
+        <h4 class="text-center font-bold pb-4 text-lg"><u>POSITION INFORMATION</u></h4>
+        <table class="min-w-full table-auto border-collapse">
+        <thead class="bg-gray-200">
+          <tr>
+            <th class="text-center py-2 px-3 md:py-3 md:px-6 border-b text-left">ID</th>
+            <th class="text-center py-2 px-3 md:py-3 md:px-6 border-b text-left">NAME</th>
+            <th class="text-center py-2 px-3 md:py-3 md:px-6 border-b text-left">ALIAS</th>
+            <th class="text-center py-2 px-3 md:py-3 md:px-6 border-b text-left">ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($position as $position)
+          <tr class="{{ $loop->even ? 'bg-gray-100' : 'bg-white' }}">
+            <td class="text-center py-2 px-3 md:py-3 md:px-6 border-b">{{ $loop->iteration }}</td>
+            <td class="text-center py-2 px-3 md:py-3 md:px-6 border-b">{{ $position->position_name }}</td>
+            <td class="text-center py-2 px-3 md:py-3 md:px-6 border-b">{{ $position->position_alias }}</td>
+            <td class="py-3 border border-white">
+            <button class="relative bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white py-2 px-4 rounded-md focus:outline-none transition duration-150 ease-in-out group "  onclick="openPositionEditPopup({{ $position->position_id }}, '{{$position->position_name ?? 'null'}}', '{{$position->position_alias ?? 'null'}}')">
+                  <i class="fas fa-edit fa-xs"></i>
+                  <span class="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-1 px-2 py-1 text-xs text-white bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">Edit</span>
+                </button>
+                <button class="relative bg-red-500 hover:bg-red-600 active:bg-red-700 text-white py-2 px-4 rounded-md focus:outline-none transition duration-150 ease-in-out group " onclick="if(confirm('{{ __('Are you sure you want to delete?') }}')) { window.location.href='position/destroy/{{$position->position_id}}'; }">
+                  <i class="fas fa-trash-alt fa-xs"></i>
+                  <span class="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-1 px-2 py-1 text-xs text-white bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out" >Delete</span>
+                </button>
+                <button class="relative bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white py-2 px-4 rounded-md focus:outline-none transition duration-150 ease-in-out group"
+                  onclick="toggleActive(this, {{ $position->position_id }})"
+                  onmouseover="setHover(this, true)"
+                  onmouseout="setHover(this, false)"
+                  style="background-color: {{ $position->status === 'Active' ? '#008000' : '#f00' }}; color: white;">
+                  <i class="fas {{ $position->status === 'Active' ? 'fa-toggle-on' : 'fa-toggle-off' }}"></i>
+                  <span class="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 text-xs text-white bg-gray-600 px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                      {{ $position->status === 'Active' ? 'Active' : 'Inactive' }}
+                  </span>
+              </button>
+          </td>
+          </tr>
+          @endforeach
+        </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  @include('popups.create-position-popup')
+  @include('popups.edit-position-popup')
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<script>
+  document.getElementById('createPosition').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('createPositionPopup').classList.remove('hidden');
+    });
+    function openPositionEditPopup(position_id, position_name, position_alias) {
+    document.getElementById('editposition_id').value = position_id;
+    document.getElementById('editposition_name').value = position_name;
+    document.getElementById('editposition_alias').value = position_alias;
+    document.getElementById('editPositionForm').action = `/position/${position_id}`;
+    document.getElementById('editPositionPopup').classList.remove('hidden');
+}
+
+function toggleActive(button, positionId) {
+    const icon = button.querySelector('i');
+    const currentStatus = icon.classList.contains('fa-toggle-on') ? 'Active' : 'Inactive';
+    const newStatus = currentStatus === 'Active' ? 'Inactive' : 'Active';
+
+    fetch(`/position/${positionId}/toggle-status`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: JSON.stringify({ status: newStatus })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            if (newStatus === 'Active') {
+                icon.classList.replace('fa-toggle-off', 'fa-toggle-on');
+                button.style.backgroundColor = '#008000'; 
+            } else {
+                icon.classList.replace('fa-toggle-on', 'fa-toggle-off');
+                button.style.backgroundColor = '#f00'; 
+            }
+        } else {
+            alert("Unable to change status.");
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function setHover(button, isHover) {
+    const icon = button.querySelector('i');
+    const statusText = button.querySelector('span');
+    
+    if (icon.classList.contains('fa-toggle-on')) {
+        button.style.backgroundColor = isHover ? '#006400' : '#008000';
+        statusText.textContent = 'Active';
+    } else {
+        button.style.backgroundColor = isHover ? '#a11' : '#f00';
+        statusText.textContent = 'Inactive';
+    }
+}
+</script>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bsicolor: {
+          DEFAULT: '#FF6608',
+        },
+        primary: {
+          DEFAULT: '#0066CB',
+          foreground: '#ffffff',
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
+        secondary: {
+          DEFAULT: '#6B7280',
+          foreground: '#F9FAFB',
+          100: '#E5E7EB',
+          200: '#D1D5DB',
+          300: '#9CA3AF',
+          400: '#6B7280',
+          500: '#4B5563',
+          600: '#374151',
+          700: '#1F2937',
+          800: '#111827',
+          900: '#000000',
+        },
+      },
+      fontFamily: {
+        'times': ['"Times New Roman"', 'serif'],
+      },
+      spacing: {
+        '22': '90px',
+        '24': '100px',
+      },
+    },
+  },
+  plugins: [],
+}
